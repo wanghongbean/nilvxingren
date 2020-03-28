2d481561e3bd30dd5a0a2000c1357af324f10ad1
Whongb <625688265@qq.com>
2020-03-28 18:58:26 +0800

    实现单生产者多消费者

--- juc-demo/src/com/nlxr/juc/Main.java
+++ juc-demo/src/com/nlxr/juc/Main.java
@@ -1,12 +1,54 @@
 package com.nlxr.juc;
 
+import com.nlxr.juc.bo.StudentBo;
+import com.nlxr.juc.handler.ConsumerHandler;
+import com.nlxr.juc.handler.ProducerHandler;
+import com.nlxr.juc.thread.SingleProducerMultiConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
-
-    public static void main(String[] args) {
+    /*
+     * 模拟任务：计算每个学生总分
+     */
+    public static void main(String[] args)throws Exception {
+        long start = System.currentTimeMillis();
+        Random random = new Random();
         //单生产者多消费者应用
+        ProducerHandler<StudentBo> producerHandler = (blockingQueue) -> {
+            List<StudentBo> studentBos = new ArrayList<>();
+            while (true) {
+            //模拟 查询db 加工数据
+                StudentBo bo = new StudentBo(random.nextInt(100), random.nextInt(100), "student" + random.nextInt(10000));
+                studentBos.add(bo);
+                try {
+                    blockingQueue.put(bo);
+                } catch (InterruptedException e) {
+                    //ignore
+                }
+                //模拟生产数据结束
+                if (studentBos.size()>10000){
+                    break;
+                }
+            }
+        };
 
+        ConsumerHandler<StudentBo> consumerHandler = (studentBo) ->{
+            System.out.println(String.format("学生名称：%s  课程A分数：%s  课程B分数：%s 总分：%s ",
+                    studentBo.hashCode(),
+                    studentBo.getAScore(),
+                    studentBo.getBScore(),
+                    studentBo.getAScore()+studentBo.getBScore()));
+        };
+        new SingleProducerMultiConsumer<>(500, producerHandler,consumerHandler,
+                "student-score_",new StudentBo(),10,new CountDownLatch(10))
+        .runUtillDone();
 
-//        new SingleProducerMultiConsumer<>();
+        long end = System.currentTimeMillis();
+        System.out.println("程序运行时间："+(end-start)/1000 +" s");
 
     }
 }
---
+++ juc-demo/src/com/nlxr/juc/bo/StudentBo.java
@@ -0,0 +1,57 @@
+package com.nlxr.juc.bo;
+
+import java.util.Objects;
+
+public class StudentBo {
+    private int aScore;
+    private int BScore;
+    private String name;
+
+    public StudentBo() {
+    }
+
+    public StudentBo(int aScore, int BScore, String name) {
+        this.aScore = aScore;
+        this.BScore = BScore;
+        this.name = name;
+    }
+
+    public int getAScore() {
+        return aScore;
+    }
+
+    public void setAScore(int aScore) {
+        this.aScore = aScore;
+    }
+
+    public int getBScore() {
+        return BScore;
+    }
+
+    public void setBScore(int BScore) {
+        this.BScore = BScore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentBo studentBo = (StudentBo) o;
+        return aScore == studentBo.aScore &&
+                BScore == studentBo.BScore &&
+                Objects.equals(name, studentBo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aScore, BScore, name);
+    }
+}
