095e10a5fa0de399d0caa24485166e2fce5a4394
labu <labu@helijia.com>
2020-07-19 11:38:14 +0800

    leetcode03--

--- demo-web/src/main/java/com/whb/demo/web/PythonDemoController.java
+++ demo-web/src/main/java/com/whb/demo/web/PythonDemoController.java
@@ -1,12 +1,8 @@
 package com.whb.demo.web;
 
 import com.whb.demo.validator.dto.RspDTO;
-import com.whb.demo.validator.dto.UserDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PythonDemoController {
-    private static final Logger LOG = LoggerFactory.getLogger(PythonDemoController.class);
+    private static final Logger LOG =
+            LoggerFactory.getLogger(PythonDemoController.class);
 
     @RequestMapping(value = "/user",method = RequestMethod.POST)
     public RspDTO<String> saveUser(String firstname,String lastname){
